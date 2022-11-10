ae16f8c58f473b2487182c676f1336cfbda1b796
Rostyslav Haluzinskiy <rossgaluzinskiy@gmail.com>
2022-11-10 12:24:16 +0300

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const refs = {
+    start: document.querySelector('button[data-start]'),
+    stop: document.querySelector('button[data-stop]'),
+}
 
+refs.start.addEventListener('click', onClickStart);
+refs.stop.addEventListener('click', onClickStop);
+let intervalId = null;
+
+function onClickStart(evt) { 
+  refs.start.setAttribute("disabled", true)
+  intervalId = setInterval(() => {
+  document.body.style.backgroundColor = getRandomHexColor()
+   }, 1000)
+  
+}
+
+function onClickStop(evt) {
+  refs.start.removeAttribute('disabled')
+  clearInterval(intervalId)
+  }
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
