abfe513e4b3c93b07650bdde4b25774f909f96a8
Rostyslav Haluzinskiy <rossgaluzinskiy@gmail.com>
2022-11-13 23:48:53 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,5 +1,5 @@
 import Notiflix from 'notiflix';
-console.log(Notiflix)
+
 const form = document.querySelector('.form');
 let firstDelay = null;
 let delayStep = null;
@@ -30,7 +30,7 @@ function counterPromise(evt) {
   delayStep = Number(step.value);
   amountPromise = Number(amount.value);
 
-  for (i = 1; i <= amountPromise; i += 1){
+  for (let i = 1; i <= amountPromise; i += 1){
      createPromise(i, firstDelay)
   .then(({ position, delay }) => {
     Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
